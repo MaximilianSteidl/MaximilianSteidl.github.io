90ae8d4a12b5471e9c83873f0bf55aaadac9bc33
Goricanec David <David.Goricanec@andritz.com>
2019-11-15 17:44:13 +0100

    commit

--- list/js/main.js
+++ list/js/main.js
@@ -1,10 +1,29 @@
-alert( "json" );
-$.getJSON("list/js/student.json")
-    .done(function( data ) {
-       console.log('logging data ' + data)
-    });
+console.log( "json" );
+/*
+function loadJSON(path, callback) {
+    console.log("path: " + path); 
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', path, false);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
 
 
-$.getJSON("list/js/student.json", function(json) {
-    console.log('logging json ' + json); 
-}); 
+var json;
+loadJSON("js/student.json", function(response) {
+	json = JSON.parse(response);
+	console.log('Here we go');
+	console.log(json[0].Students[0].id); // Successfully shows the result
+});
+console.log('Here we go2');
+console.log(json[0].Students[0].id); // TypeError: json is undefined
+*/
+
+var mydata = JSON.parse(student);
+ alert(mydata[0].id);
+ alert(mydata[0].vorname);
