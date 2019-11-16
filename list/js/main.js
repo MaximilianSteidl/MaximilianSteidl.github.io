c587b4ff5bfad7423724ab4815c04a2f11d0a735
grzth149 <thomas.reiter@andritz.com>
2019-11-16 14:19:29 +0100

    main.js update

--- list/js/main.js
+++ list/js/main.js
@@ -1,11 +1,24 @@
-alert( "json" );
+var json_data = [];
 
-$.getJSON("https://github.com/MaximilianSteidl/maximiliansteidl.github.io/blob/master/list/js/student.json")
-    .done(function( data ) {
-       console.log('logging data ' + data)
-    });
+window.addEventListener("load", function () {
+	function ajaxLoadData() {
+		if (this.readyState == 4 && this.status == 200) {
+				json_data = JSON.parse(this.responseText);
+				console.log("JSON:");
+				console.log(this.responseText);
+				console.log(json_data);
+				console.log(json_data[1].vorname);
+		}
+	}
 
+	function loadJSON() {
+		var xhr = new XMLHttpRequest();
+		var path = "https://raw.githubusercontent.com/ozwoldFH/webapp_inventory_WS2019/master/data/data.json";
+		xhr.onreadystatechange = ajaxLoadData;
+		xhr.open("GET", path, false);
+		xhr.send();
+	}
 
-$.getJSON("https://github.com/MaximilianSteidl/maximiliansteidl.github.io/blob/master/list/js/student.json", function(json) {
-    console.log('logging json ' + json); 
-}); 
+	loadJSON();
+	
+});
