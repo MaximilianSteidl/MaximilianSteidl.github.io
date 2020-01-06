51406ff603f184017bde537004f22249eab7c6ef
Goricanec David <David.Goricanec@andritz.com>
2020-01-06 21:35:28 +0100

    added export

--- Sprint3/list/js/main.js
+++ Sprint3/list/js/main.js
@@ -108,3 +108,77 @@ function user_delete(uebergabe_id)
 		});	
 	}
 }
+
+
+
+
+//export
+function convertToCSV(objArray) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var str = '';
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+			if (index == "__v" || index == "_id")
+			{
+				continue;
+			}
+            if (line != '') line += ','
+
+            line += array[i][index];
+        }
+
+        str += line + '\r\n';
+    }
+
+    return str;
+}
+
+function exportCSVFile(headers, items, fileTitle) {
+    if (headers) {
+        items.unshift(headers);
+    }
+
+    // Convert Object to JSON
+    var jsonObject = JSON.stringify(items);
+
+    var csv = this.convertToCSV(jsonObject);
+
+    var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+
+    var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, exportedFilenmae);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", exportedFilenmae);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
+
+function export_students()
+{
+	$.get("http://localhost:8080/exportStudents").done(function( data ) {
+		//console.log( data );
+		var headers = {
+			Student_id: "Studenten Nummer",
+			vorname: "Vorname",
+			nachname: "Nachname",
+			studiengang: "Studiengang",
+			wohnort:"Wohnort",
+			semester:"Semester",
+			Geburtsdatum: "Geburtsdatum"
+		};
+		var fileTitle = 'Studentenliste';
+		exportCSVFile(headers, data, fileTitle);
+	});
+}
